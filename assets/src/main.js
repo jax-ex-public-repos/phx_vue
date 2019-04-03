6488c9ef8984b1b0f0ad791f60f8cb5620f8fd73
michael dimmitt <michaelgdimmitt@gmail.com>
2019-04-03 12:53:17 -0400

    Not working because ... see note - begin ... replace vue with vue-cli

--- assets/src/main.js
+++ assets/src/main.js
@@ -1,6 +1,8 @@
-// assets/src/main.js
-import Vue from 'vue'
-import App from './App'
-Vue.config.productionTip = false
-const element = document.getElementById('app')
-const app = new Vue(App).$mount(element)
+import Vue from 'vue';
+import App from './App.vue';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app');
